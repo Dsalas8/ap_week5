30f48f6a351fed364024cafd231b01fc724c5de3
Dsalas8 <ssdamian1218@gmail.com>
2025-11-05 21:07:48 +0000

    edits

--- week5_bellRinger.py
+++ week5_bellRinger.py
@@ -27,21 +27,37 @@ print(personality_name)
 # Manipulating Words:
 # Given the string info = "Python is fun. Fun is good. Good is subjective.",
 # a. Extract the word 'subjective' without knowing its exact position.
+quote = "Python is fun. Fun is good. Good is subjective."
+print(quote.find('subjective'))
+adjective = quote[36:]
+print(adjective)
 # b. Extract every third word.
+every_third_word = quote.split()[::3]
+print(every_third_word)
 # c. Reverse the positions of the words, but keep the characters in each word in the same order.
-
+reversed_word_positions = quote.split()[::1]
+print(reversed_word_positions)
 # Problem Set 3: String Methods
 # Upper & Lower:
 # Convert the following text to lowercase: "MAY THE FORCE BE WITH YOU."
+text = "MAY THE FORCE BE WITH YOU"
+lowercase_text = print(text.lower())
 
 # String Joining and Splitting:
 # Given the list motto = ["Make", "haste", "slowly."],
 # a. Convert the list into a single string.
+motto =["Make", "Haste", "slowly"]
+motto_string = ' '.join(motto)
+print(motto_string)
 # b. Now, split the string at every occurrence of the letter 'a'.
-
+every_letter_a = motto_string.split('a')
+print(every_letter_a)
 # Replacing Words:
 # Modify the sentence: "Life is what happens when you are busy making other plans."
 # a. Replace "busy" with "distracted".
+Life = "Life is what happens when you are busy making other plans"
+new_life = Life.replace("plans","mistakes") 
+print(new_life)
 # b. Replace "plans" with "mistakes".
 
 # Problem Set 4: String Properties and Advanced Operations
@@ -50,7 +66,12 @@ print(personality_name)
 
 # Word Search:
 # Check if the word "moonlight" appears in the quote: "With freedom, books, flowers, and the moon, who could not be happy? - Oscar Wilde"
-
+word = "moonlight"
+quote = "With freedom, books, flowers, and the moon, who could not be happy? - Oscar Wilde"
+appears_in_quote = word in quote
+print(appears_in_quote)
 # Length and Count:
 # a. Calculate the number of characters (including spaces and punctuation) in the word/phrase: "Supercalifragilisticexpialidocious".
 # b. Count the number of times the letter 'i' appears in the same word/phrase.
+count_of_i = phrase.count('i')
+print(count_of_i)

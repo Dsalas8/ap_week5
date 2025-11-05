e55cf8dae2cd89923980c866c47139f15840d18e
Dsalas8 <ssdamian1218@gmail.com>
2025-11-05 20:12:21 +0000

    edits

--- week5_bellRinger.py
+++ week5_bellRinger.py
@@ -20,7 +20,10 @@ reversed_alphabet = alphabet[::1]
 # Problem Set 2: Extracting Information
 # From Descriptions:
 # Extract the name of the famous personality from the quote "Ask not what your country can do for you — ask what you can do for your country. - John F. Kennedy"
-
+quote = "Ask not what your country can do for you — ask what you can do for your country. - John F. Kennedy"
+print(quote.find('John F. Kennedy'))# out put 83
+personality_name = quote[83:] 
+print(personality_name)
 # Manipulating Words:
 # Given the string info = "Python is fun. Fun is good. Good is subjective.",
 # a. Extract the word 'subjective' without knowing its exact position.
